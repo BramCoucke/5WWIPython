48408436a0b5c2d45da40100df41bd0903a3a3ac
BramCoucke <bram.coucke1@sgsintpaulus.eu>
2019-09-20 11:25:31 +0200

    test variabelen

---
+++ 04-variabelen/test.py
@@ -0,0 +1,11 @@
+#wisselen van waarde
+#x, y = 2, 'hallo'
+#x, y = y, x
+#print(x,y)
+
+a= float(input('a:  '))
+b= float(input('b:  '))
+c= float(input('c:  '))
+
+discriminant = b**2 - (4*a*c)
+print(discriminant)
